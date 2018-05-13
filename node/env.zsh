c33585df5c18fe89be7f2acf6cbd08049726fa14
Kirk Stork <kastork@nps.edu>
2018-05-13 06:29:45 -0700

    improve nvm config

---
+++ node/env.zsh
@@ -0,0 +1,5 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "Using node lts/carbon by default"
+nvm use lts/carbon
---
+++ node/load.sh
@@ -0,0 +1,4 @@
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# echo "Using node lts/carbon by default"
+# nvm use lts/carbon
