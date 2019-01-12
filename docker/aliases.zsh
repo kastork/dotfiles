95c383ad93a151bf1a6ce18ca9cbf029edd3cd46
Kirk Stork <kastork@nps.edu>
2019-01-12 06:33:48 -0800

    Cleaning up

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,4 @@
 alias d='docker $*'
 alias d-c='docker-compose $*'
+alias d-purge-containers="docker rm $(docker ps -a | grep Exited | awk '{print $1}')"
+alias d-purge-images='docker rmi $(docker images -q --filter "dangling=true")'
