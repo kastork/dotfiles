b6db3058ac2e537289994f5042cd4b39bd549e8f
Kirk Stork <kastork@nps.edu>
2019-06-15 08:12:42 -0700

    move anaconda off root directory

--- anaconda/aliases.zsh
+++ anaconda/aliases.zsh
@@ -1,17 +1,17 @@
 use_conda() {
     echo "Using Anaconda"
-    __conda_setup="$(CONDA_REPORT_ERRORS=false '/anaconda3/bin/conda' shell.bash hook)"
+    __conda_setup="$(CONDA_REPORT_ERRORS=false 'anaconda3/bin/conda' shell.bash hook)"
     
      # 2> /dev/null)"
     
     if [ $? -eq 0 ]; then
         \eval "$__conda_setup"
     else
-        if [ -f "/anaconda3/etc/profile.d/conda.sh" ]; then
-            . "/anaconda3/etc/profile.d/conda.sh"
+        if [ -f "anaconda3/etc/profile.d/conda.sh" ]; then
+            . "anaconda3/etc/profile.d/conda.sh"
             CONDA_CHANGEPS1=false conda activate base
         else
-            \export PATH="/anaconda3/bin:$PATH"
+            \export PATH="anaconda3/bin:$PATH"
         fi
     fi
     unset __conda_setup
