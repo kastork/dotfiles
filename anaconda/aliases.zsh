f8181390c0dddf94d6f67547b19278e574400411
Kirk Stork <kastork@nps.edu>
2019-06-15 08:04:09 -0700

    update anaconda setup

--- anaconda/aliases.zsh
+++ anaconda/aliases.zsh
@@ -1,1 +1,20 @@
-alias useconda='export PATH=~/anaconda3/bin:$PATH'
+use_conda() {
+    echo "Using Anaconda"
+    __conda_setup="$(CONDA_REPORT_ERRORS=false '/anaconda3/bin/conda' shell.bash hook)"
+    
+     # 2> /dev/null)"
+    
+    if [ $? -eq 0 ]; then
+        \eval "$__conda_setup"
+    else
+        if [ -f "/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "/anaconda3/etc/profile.d/conda.sh"
+            CONDA_CHANGEPS1=false conda activate base
+        else
+            \export PATH="/anaconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+}
+
+alias useconda=use_conda
