142e5d748467c78ca93626e956e637f340546399
Kirk Stork <kastork@nps.edu>
2019-10-01 07:16:02 -0700

    Make the conda base env automatic

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias buildenv='while read requirement; do conda install --yes $requirement; done < requirements.txt'
