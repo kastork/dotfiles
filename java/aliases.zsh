904d487c859bd8cbf6ca46d136b866998a6439f5
Kirk Stork <kastork@nps.edu>
2020-10-09 05:46:26 -0700

    update java aliases

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,3 +1,3 @@
-alias j8='export JAVA_HOME=$(/usr/libexec/java_home -version 1.8)'
+alias j8='export JAVA_HOME=$(/Users/kirk/.sdkman/candidates/java/8.0.265.j9-adpt -version 1.8)'
 alias j11='export JAVA_HOME=$(/usr/libexec/java_home -version 11)'
 alias j14='export JAVA_HOME=$(/usr/libexec/java_home -version 14)'
