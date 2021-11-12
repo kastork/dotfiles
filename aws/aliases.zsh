780d8d91f50edc02be7f4fa770fff221f0d7f008
Kirk Stork <kastork@nps.edu>
2021-11-12 06:21:54 -0800

    fiddling with aliases

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,5 +1,7 @@
 alias mpgov='export AWS_PROFILE=mp-gov'
 alias eccogov='export AWS_PROFILE=ecco-gov'
+alias movesgov='export AWS_PROFILE=lincolnlabs-ops'
 alias eccocom='export AWS_PROFILE=nps-ops'
+alias perscom='export AWS_PROFILE=zenuevo-admin'
 
 
