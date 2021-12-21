260bc85f176171c1010019f05c58e0d713ed428c
Kirk Stork <kastork@nps.edu>
2021-12-21 15:02:07 -0800

    conda and java tweaks

--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,3 +1,2 @@
-alias j8='export JAVA_HOME=/Users/kirk/.sdkman/candidates/java/8.0.265-amzn'
-alias j11='export JAVA_HOME=/Users/kirk/.sdkman/candidates/java/11.0.8-amzn'
-alias j15='export JAVA_HOME=/Users/kirk/.sdkman/candidates/java/15.0.2.7.1-amzn'
+alias j8='export JAVA_HOME=/Users/kirk/.sdkman/candidates/java/8.312.07.1-amzn'
+alias j11='export JAVA_HOME=/Users/kirk/.sdkman/candidates/java/11.0.13.8.1-amzn'
