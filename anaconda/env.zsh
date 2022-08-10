44a0bc6a3c516bd2d39a85242a0bb4c59cb6a1fc
Kirk Stork <kastork@nps.edu>
2022-08-10 08:19:07 -0700

    adding a timestamp file to push-raid-work

--- anaconda/env.zsh
+++ anaconda/env.zsh
@@ -1,15 +1,14 @@
-# configure conda
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/local/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/Users/kirk/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/Users/kirk/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/kirk/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/usr/local/anaconda3/bin:$PATH"
+        export PATH="/Users/kirk/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
