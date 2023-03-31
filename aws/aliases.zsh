7670fa11f415c0998c8eb46e05a1df6cde4aa24f
Kirk Stork <kastork@nps.edu>
2023-03-31 08:35:44 -0700

    tweaking sync scripts

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,10 +1,11 @@
 alias mpgov='export AWS_PROFILE=mp-gov'
 alias eccogov='export AWS_PROFILE=ecco-gov'
 alias movesgov='export AWS_PROFILE=lincolnlabs-ops'
-alias eccocom='export AWS_PROFILE=nps-ops'
+alias eccocomw1='export AWS_PROFILE=nps-ops-w1'
+alias eccocomw2='export AWS_PROFILE=nps-ops-w2'
 alias perscom='export AWS_PROFILE=zenuevo-admin'
 alias west1='export AWS_REGION=us-west-1'
-alias west1='export AWS_REGION=us-west-2'
+alias west2='export AWS_REGION=us-west-2'
 alias east1='export AWS_REGION=us-east-1'
 alias govwest='export AWS_REGION=us-gov-west-1'
 
